5e0e04552565481080768443496a89ef701ab843
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-03-27 19:23:00 +0000

    fixed users.nix

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -1,9 +1,8 @@
-{config, ...}:{
+{...}:{
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nova = {
     isNormalUser = true;
     description = "Nova";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
   };
 }
